a250220929dba601ca4c979e327689a658768b2d
Luke Southam <luke@devthe.com>
2014-07-10 00:18:09 +0100

    fixing terrible styling

--- scripts/date.py
+++ scripts/date.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python
-
 """
 Simply outputs the time/date.
 
 NOTE: does not check __name__ == '__main__'
 """
 
-from time import time,ctime
+from time import time, ctime
 
-print 'Today is',ctime(time())
-
-
+print 'Today is', ctime(time())
